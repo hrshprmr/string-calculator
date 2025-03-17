3f78fc1f4019a49ecda63096f33bbd3bbd95e349
Harsh <harsh.prmr1@gmail.com>
2025-03-17 13:57:36 +0530

    handle new line as delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,5 @@
 require './string_calculator'
+require 'pry'
 StringCalculator
 RSpec.describe StringCalculator do
   describe '.add' do
@@ -16,6 +17,10 @@ RSpec.describe StringCalculator do
 
     it 'returns sum of multiple numbers separated by commas' do
       expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
+    end
+
+    it 'allows new lines as delimiters' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
   end
 end
